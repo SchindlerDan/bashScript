db44758ced4ffb369001b02b9b3b6587c6f6f45d
Daniel Schindler <schindld@dc18.cis.gvsu.edu>
2018-04-10 12:19:16 -0400

    successfully backs up single files. Pushing so Josh can play with it -Dan Schindler

--- backup.sh
+++ backup.sh
@@ -2,38 +2,18 @@
 
 for input in $*
 do
-	if [ $input = "-c"  ]
-		then
-        	echo "The number of files:"
-        	ls| wc -l
-        	echo "how many bytes:"
-        	du -b .
-        	
-	elif [ $input = "-l" ]
-        	then
-        	echo "List all the files"
-        	ls -a
-	elif [ "--help" = $input ]
-		then
-        	echo "How to run this script"
-        	echo "-c will give you the number of files and how many bytes."
-        	echo "-l will list all files"
-	elif [ "-lt2" = "$s" ]
-        	then
-        	echo "Complete"
-	
-	fi
-	if test -d  ~/Backup
+
+	if [[ -d  ~/Backup ]]
 		then
         	echo
 	else
         	mkdir ~/Backup
 	fi
 	
-	if test -f $input
+	if [[ -f $input && -e $input ]]
 		then
 		cp $input ~/Backup
-	elif test -d $input
+	elif [[ -d $input && -e $input ]]
 		then
 		cp $input ~/Backup -r
 	fi
@@ -42,3 +22,31 @@ do
 	
 done
 
+
+
+
+
+for input in $*
+do
+	if [[ $input = "-c" ]]
+                then
+                echo "The number of files:"
+                ls| wc -l
+                echo "how many bytes:"
+                du -b .
+
+        elif [ $input = "-l" ]
+                then
+                echo "List all the files"
+                ls -a
+        elif [ "--help" = $input ]
+                then
+                echo "How to run this script"
+                echo "-c will give you the number of files and how many bytes."
+                echo "-l will list all files"
+        elif [ "-lt2" = "$s" ]
+                then
+                echo "Complete"
+
+        fi
+done
